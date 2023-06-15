3840e2fe85d7767163c1315ef5c964b92fcad399
ANDY CHEN <chen829015@gmail.com>
2023-06-15 22:16:07 +0800

    update

--- chat.py
+++ chat.py
@@ -1,6 +1,6 @@
 def read_file(filename):
 	lines = []
-	with open(filename, 'r', encoding='ust-8-sig') as f:
+	with open(filename, 'r', encoding='utf-8-sig') as f:
 		for line in f:
 			lines.append(line.strip())
 	return lines
